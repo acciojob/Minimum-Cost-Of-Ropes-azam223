671f71bb1e700a60aa024b47b7cf712475f89e3d
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 15:20:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function calculateMinCost() {
   const inputElement = document.getElementById('rope-lengths');
   const resultElement = document.getElementById('result');
@@ -24,73 +25,3 @@ function calculateMinCost() {
 
   resultElement.textContent = 'Minimum cost is ' + totalCost;
 }
-
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  insert(value) {
-    this.heap.push(value);
-    this.bubbleUp();
-  }
-
-  extractMin() {
-    if (this.size() === 0) {
-      return null;
-    }
-    if (this.size() === 1) {
-      return this.heap.pop();
-    }
-
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.sinkDown(0);
-    return min;
-  }
-
-  bubbleUp() {
-    let index = this.size() - 1;
-    while (index > 0) {
-      const element = this.heap[index];
-      const parentIndex = Math.floor((index - 1) / 2);
-      const parent = this.heap[parentIndex];
-
-      if (element >= parent) {
-        break;
-      }
-
-      this.heap[index] = parent;
-      this.heap[parentIndex] = element;
-      index = parentIndex;
-    }
-  }
-
-  sinkDown(index) {
-    const left = 2 * index + 1;
-    const right = 2 * index + 2;
-    let smallest = index;
-
-    if (left < this.size() && this.heap[left] < this.heap[smallest]) {
-      smallest = left;
-    }
-
-    if (right < this.size() && this.heap[right] < this.heap[smallest]) {
-      smallest = right;
-    }
-
-    if (smallest !== index) {
-      const temp = this.heap[index];
-      this.heap[index] = this.heap[smallest];
-      this.heap[smallest] = temp;
-      this.sinkDown(smallest);
-    }
-  }
-}
-
-// Export the MinHeap class for use in the HTML file
-window.MinHeap = MinHeap;
